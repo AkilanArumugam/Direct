3f6077aafd2b3bdfa9e2eefd560890a4b41980a5
AkilanArumugam <ozotec.nmkl@gmail.com>
2023-01-22 22:55:32 +0530

    newmessage

---
+++ src/com/sample/Employee.java
@@ -0,0 +1,16 @@
+package com.sample;
+
+public class Employee {
+	
+	private void empData() {
+		
+		System.out.println("employee name is Akilan");
+	}
+
+	public static void main(String[] args) {
+		
+		Employee employee = new Employee();
+		employee.empData();
+		
+	}
+}
